090888a4d2cb0e5deb6eded8b164cdb2c935f507
Adam Rudy <awsrudy@amazon.com>
2021-12-09 15:00:58 -0500

    Basic image generation endpoint is now working
    The endpoint accepts a day of the week at this point in time

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Mind Tether is an app designed to provide small reminders that can help you feel
 
 
 The current design will take advantage of the programability of iPhones through Shortcuts. The shortcut will be created and set to run on a schedule. It will have dynamic variables it will pass. In AWS, a unique phone background image is created, saved in S3 and a pre-signed URL is generated. The shortcut will then download the file from the provided background URL. The S3 lifecycle policies will expire images after 60 seconds. 
+
+--- 
+
+Some additional notes:
+
+Because the Presigned URL for the S3 object is so long, it gets truncated in the API response. To solve for that I have implemented a URL shortner solution as described in [this](https://aws.amazon.com/blogs/compute/build-a-serverless-private-url-shortener/) blog post. While this is effective, the shortener should be integrated directly into this application to reduce latency and cost while improving effeciency and simplicity. 
+
+In the meantime, using a third-party shortner has allowed for me to push those changes to post-MVP
--- lambda/generate_helper_image.py
+++ lambda/generate_helper_image.py
@@ -1,12 +1,19 @@
-import uuid
+import requests
 import boto3
-from PIL import Image
+import requests
+import json
+
+from PIL import (
+    Image, 
+    ImageDraw, 
+    ImageFont
+    )
 import base64
 import os
 from io import BytesIO
 from uuid import uuid4
 
-days={
+days= dict({
     'sunday' :{
         'color': (165,209,200)
     },
@@ -28,39 +35,72 @@ days={
     'saturday':{
         'color': (183,181,204)
     }
-}
+})
+
 def upload_image_to_s3 (uuid_path, img):
-    bucket_name = os.environ['S3_BUCKET']
+    # bucket_name = os.environ['S3_BUCKET']
+    bucket_name="phonebackgroundhelperstack-assetbucket1d025086-q1yucn7kpzas"
     s3 = boto3.client("s3")
     s3_response = s3.put_object(Bucket=bucket_name,
                   Key=uuid_path,
                   Body=img)
-    print(s3_response)
-    return uuid_path;
+    s3_presigned_response = s3.generate_presigned_url(
+        'get_object', Params = {
+            'Bucket': bucket_name,
+            'Key': uuid_path
+        }, ExpiresIn=60
+    )
+    return s3_presigned_response
 
+def _get_base_day_image(day: str, phone_model: str):
+    #TODO Validate day string and phone_model strings are approved. 
+    return Image.open("/opt/images/daily_background/%s@%s.jpg" % (day,phone_model))
+
+def _generate_day_text_image(day:str, background: Image):
+    fnt = ImageFont.truetype("/opt/fonts/theboldfont.ttf", 200)
+    draw = ImageDraw.Draw(background,"RGB")
+    draw.text((25,2000),day.capitalize(),fill=(255,255,255), font=fnt)
+    return background
+
+def generate_image(*args, day:str=None, phone_model:str=None):
+    background = _get_base_day_image(day,phone_model)
+    _generate_day_text_image(day,background)
+    return background
+
+def shorten_url(img_url:str):
+    # print("IMAGE URL IS\n %s" % (img_url))
+    response = requests.post("https://link.mindtether.rudy.fyi/admin_shrink_url",
+    json={
+        "url_long": img_url,
+        "cdn_prefix": "link.mindtether.rudy.fyi"
+    },allow_redirects=True)
+    response_values = json.loads(response.text)
+    if response.status_code == 200:
+        response.close()
+        return response_values['url_short']
+    else:
+        response.close()
+        return response.status_code
 
 def lambda_handler(event, context):
-    print("layer info: %s" % (os.listdir("/opt/")))
-
+    print("EVENT object = %s" % (event))
+    phone_model="iphone13pro"
     width = 1170
     height = 2532
-    day = event['body']['day']
-    print('day requested is %s' % (day))
-    # uuid_name = "%s.jpeg" % (uuid4())
-    uuid_name = '%s@iPhone13Pro' % (day)
-    uuid_path = "/assets/images/daily-background/%s" % uuid_name
-    print("color is %" % (days[day]))
-    print("color is %" % (days[day]['color']))
-    img = Image.new("RGB", [width,height],color=days[day]['color'])
-    img.format = "JPEG"
+    day = event['queryStringParameters']['day']
+    uuid_name = "%s.jpeg" % (uuid4())
+    uuid_path = "images/daily-background/%s" % uuid_name
     buffer = BytesIO()
+    img = generate_image(day=day,phone_model=phone_model)
     img.save(buffer, "JPEG")
     img.close()
     buffer.seek(0)
     image_url = upload_image_to_s3(uuid_path, buffer)
+    image_url = shorten_url(image_url)
     return {
         'statusCode': 200,
         'body': {
-            "ImageUUID":uuid_name
+            'image_url': image_url
         }
     }
+    
