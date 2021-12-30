cc692d857e1e4c7e952807d16e01d4a79b8037b2
Adam <developer@flamingquaks.dev>
2021-12-30 09:20:29 -0500

    Starting creating the URL shortener

--- lambda_layers/mindtether_core/python/MindTetherCore/__init__.py
+++ lambda_layers/mindtether_core/python/MindTetherCore/__init__.py
@@ -1,5 +1,8 @@
 from os import path,environ
-
+import boto3
+import botocore
+import string
+import random
 
     
     
@@ -50,3 +53,80 @@ class Day():
     @classmethod
     def get_day_color(self, day: str):
         return Day.__days[day.lower()]['color']
+    
+class URLShortener:
+    def __init__(self) -> None:
+        pass
+    
+    def shorten_url(long_url:str, short_url_bucket:str = environ['SHORT_URL_BUCKET'], short_url_host:str = ['SHORT_URL_HOST']):
+        """URLShortener.shorten_url
+        
+        This takes the long URL and generates a URL shortener link.
+        
+        NOTE: if optional values aren't provided, this method will attempt to use 
+        the caller's environment variables. Make sure they are set on the caller.
+        
+        Additionally, ensure that the calling function has the necessary permissions
+        to the AWS resources. 
+
+        Args:
+            long_url (str): URL that needs to be shortened
+            short_url_bucket (str, optional): [description]. Defaults to environ['SHORT_URL_BUCKET'].
+            short_url_host (str, optional): [description]. Defaults to ['SHORT_URL_HOST'].
+
+        Raises:
+            Exception: If necessary values are missing, an exception will be thrown
+
+        Returns:
+            [str]: Shortened URL
+        """
+        if not long_url:
+            raise Exception("Missing long URL!")
+        if not short_url_bucket or not short_url_host:
+            raise Exception("Error! Either the short_url_bucket or short_url_host values were not provided \
+                and were not set in the environment variables.")
+        
+        # generate a random string of n characters, lowercase and numbers
+        def generate_random(n):
+            return ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(n))
+
+        # checks whether an object already exists in the Amazon S3 bucket
+        # we do a head_object, if it throws a 404 error then the object does not exist
+        def exists_s3_key(s3_client, bucket, key):
+            try:
+                resp = s3_client.head_object(Bucket=bucket, Key=key)
+                return True
+            except botocore.exceptions.ClientError as e:
+                # if ListBucket access is granted, then missing file returns 404
+                if (e.response['Error']['Code'] == "404"): return False
+                # if ListBucket access is not granted, then missing file returns 403 (which is the case here)
+                if (e.response['Error']['Code'] == "403"): return False
+                print(e.response)
+                raise e 
+            
+
+
+        ### Generate a short id for the redirect
+        # check if short_key object already exists - collision could occur
+        s3 = boto3.client('s3')
+
+        while (True):
+            short_id = generate_random(7)
+            short_key = "u/" + short_id
+            if not(exists_s3_key(s3, short_url_bucket, short_key)):
+                break
+            else:
+                print("We got a short_key collision: " + short_key + ". Retrying.")
+
+        print("We got a valid short_key: " + short_key)
+
+        ### Third step: create the redirection object in the S3 bucket
+        resp = s3.put_object(Bucket=short_url_bucket,
+                            Key=short_key,
+                            Body=b"",
+                            WebsiteRedirectLocation=long_url,
+                            ContentType="text/plain")
+
+        public_short_url = "https://" + short_url_host + "/" + short_id;
+
+        return public_short_url
