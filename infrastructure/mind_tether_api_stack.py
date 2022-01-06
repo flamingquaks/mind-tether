7a72bc996a9971f47c5183a761903aeebc3cecd8
flamingquaks <developer@flamingquaks.dev>
2022-01-06 14:00:55 -0500

    Fixed typo when adding lambda layer

--- infrastructure/mind_tether_api_stack.py
+++ infrastructure/mind_tether_api_stack.py
@@ -263,7 +263,7 @@ class MindTetherApiStack(Stack):
                                                    handler="app.lambda_handler",
                                                    runtime=_lambda.Runtime.PYTHON_3_8)
         
-        get_tether_entry_lambda.add_permission(mindtether_core)
+        get_tether_entry_lambda.add_layers(mindtether_core)
         
         get_tether_requests_table.grant_read_write_data(get_tether_entry_lambda)
         get_tether_state_machine.grant_start_execution(get_tether_entry_lambda)
