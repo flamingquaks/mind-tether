f00f97a1f7df6f6c25a5ca00684dcb0571b62b53
flamingquaks <developer@flamingquaks.dev>
2022-01-06 13:31:05 -0500

    ALPHA-MERGE: Added missing commit

--- lambda/get_tether/compile_image/app.py
+++ lambda/get_tether/compile_image/app.py
@@ -8,7 +8,7 @@ from MindTetherCore import URLShortener
 
 asset_bucket = environ['MIND_TETHER_API_ASSETS']
 request_table_name = environ['REQUEST_TABLE_NAME']
-generated_assets_prefix = "get-tether/generated"
+generated_assets_prefix = "assets"
 
 def get_s3_image(key:str,bucket:str,type:str):
     s3 = boto3.client("s3")
@@ -23,6 +23,7 @@ def lambda_handler(event,context):
         "bkg_img", "day_text_img", "requestId"}:
         width = event['width']
         height = int(event['height'])
+        font = event['font']
         day = event["day"]
         bkg_img = event['bkg_img']
         request_id = event['requestId']
@@ -45,7 +46,7 @@ def lambda_handler(event,context):
         vertical_offset = ((free_space_bottom_y-free_space_top_y) - day_text_image_height)/2
         bkg_image.paste(day_text_image,(5,int(free_space_top_y+vertical_offset)),day_text_image)
         day_text_image.close()
-        image_name = "%s.jpeg"%(uuid4())
+        image_name = f"{day}@{font}@{width}x{height}.jpg"
         buffer = BytesIO()
         bkg_image.save(buffer,"JPEG")
         bkg_image.close()
@@ -57,25 +58,5 @@ def lambda_handler(event,context):
             Key=asset_key,
             Body=buffer
         )
-        s3_presigned_url = s3.generate_presigned_url(
-            'get_object', Params = {
-                'Bucket' : asset_bucket,
-                'Key': asset_key
-            }, ExpiresIn=60
-        )
-        
-        event['generatedImageURL'] = s3_presigned_url
-        short_url = URLShortener.shorten_url(s3_presigned_url)
-        dynamodb = boto3.resource("dynamodb")
-        table = dynamodb.Table(request_table_name)
-        update_response = table.update_item(
-            Key={
-                "requestId":request_id
-            },
-            UpdateExpression="set create_status=:s, image_url=:u",
-            ExpressionAttributeValues={
-                ':s': "COMPLETE",
-                ':u': short_url
-            }
-        )
+        event['asset_key'] = f"{generated_assets_prefix}/{image_name}"
         return event
